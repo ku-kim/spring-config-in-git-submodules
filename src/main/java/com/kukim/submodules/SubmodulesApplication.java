5214752583f5f0918a8fe6e8d2f7992e9856e930
“kukim” <kukim.dev@gmail.com>
2022-06-29 10:44:09 +0900

    setup: spring project

---
+++ src/main/java/com/kukim/submodules/SubmodulesApplication.java
@@ -0,0 +1,13 @@
+package com.kukim.submodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmodulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmodulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kukim/submodules/SubmodulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kukim.submodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmodulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
